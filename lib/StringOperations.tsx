f531f2f67446b6da5ad248118e46598701a9c1e7
Sascha Hameister <sascha.hameister@royal-software.de>
2019-04-18 12:03:46 +0200

    Added docs

--- lib/StringOperations.tsx
+++ lib/StringOperations.tsx
@@ -1,10 +1,27 @@
+/**
+ * StringOperations is a library class that providers static helpers to work with strings
+ */
 export class StringOperations
 {
+  /**
+   * Return a copy of the string without alphanumeric characters.
+   * 
+   * @method removeNonAlphaNumericChars
+   * @param {string} str 
+   * @return {string}
+   */
   public static removeNonAlphaNumericChars(str:string):string
   {
     return str.replace(/\W/g, '')
   }
 
+  /**
+   * Return a copy of the string with capitalized first character.
+   * 
+   * @method capitalize
+   * @param {string} str
+   * @return {string} 
+   */
   public static capitalize(str:string):string
   {
     return str.charAt(0).toUpperCase() + str.slice(1)
