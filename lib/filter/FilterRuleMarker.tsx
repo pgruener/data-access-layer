a8bcec8baeaf6f73b62663ec5c8a6afcdbbe6439
Sascha Hameister <sascha.hameister@royal-software.de>
2019-04-23 16:23:07 +0200

    Added docs

--- lib/filter/FilterRuleMarker.tsx
+++ lib/filter/FilterRuleMarker.tsx
@@ -15,16 +15,26 @@ export class FilterRuleMarker<T extends FilterRule<Object>>
     this._filterRule = filterRule
   }
 
+  /**
+   * Attribute accessor for filterRule
+   */
   get filterRule():T
   {
     return this._filterRule
   }
 
+  /**
+   * Attribute accesor for used
+   */
   get used():boolean
   {
     return this._used
   }
 
+  /**
+   * @method use
+   * @return {T} filterRule
+   */
   public use():T
   {
     this._used = true
